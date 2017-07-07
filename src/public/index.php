a8521f82aa4834a447c17acfe09950178aaa4a96
Oliver Cordes <ocordes@astro.uni-bonn.de>
2017-07-07 14:29:03 +0200

    - play a little bit

---
+++ src/public/index.php
@@ -0,0 +1,56 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+require '../vendor/autoload.php';
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "user";
+$config['db']['pass']   = "password";
+$config['db']['dbname'] = "exampleapp";
+$config['db']['dbdir']  = "sqlite://tmp/user.sqlite";
+
+$app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('path/to/templates', [
+        'cache' => 'path/to/cache'
+    ]);
+    
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    $this->logger->addInfo("Something interesting happened");
+
+    return $response;
+});
+
+
+
+$app->run();
+?>
