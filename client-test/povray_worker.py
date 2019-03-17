61c81900065e4097b7a92caa44e21fdbd7ed026c
Oliver Cordes <ocordes@astro.uni-bonn.de>
2019-03-17 18:41:26 +0100

    - work on the povray worker - documentation of file formats is necessary

--- client-test/povray_worker.py
+++ client-test/povray_worker.py
@@ -7,15 +7,21 @@ import time
 import os
 
 import tarfile
+import configparser
 
 
 class PovrayWorker(object):
-    def __init__(self, session, tempdir='.' ):
+
+    base_files_dir = 'base_files'
+    model_dir      = 'model'
+
+    def __init__(self, session, tempdir='.', default_libs=None ):
         self._session = session
 
         self._image = None
 
         self._tempdir = tempdir
+        self._default_libs = default_libs
 
 
     def _check_tmpdir(self):
@@ -39,6 +45,37 @@ class PovrayWorker(object):
 
         if self._project is None:
             return
+
+
+    def _create_master_ini(self, base_files_dirs):
+        fname = os.path.join(self._tempdir, self.model_dir, 'master.ini')
+        print('Generating master ini file ...')
+        with open(fname,'w') as master_file:
+            master_file.write('; POVRAY master file\n')
+            if self._default_libs is not None:
+                for dir in self._default_libs:
+                    master_file.write('Library_Path="%s"\n' % dir)
+            for dir in base_files_dirs:
+                master_file.write('Library_Path="%s"\n' % dir)
+
+        print( 'Master file \'%s\' successfully written!' % fname)
+
+
+    def _read_input_directories(self, subdir='.'):
+        dirs = []
+        path = os.path.join(self._tempdir, subdir)
+        with os.scandir(path) as it:
+            for entry in it:
+                if entry.name.endswith('.ini') and entry.is_file():
+                    print(entry.name)
+                    fname = os.path.join(path, entry.name)
+                    config = configparser.ConfigParser()
+                    config.read(fname)
+                    dir = config.get('DEFAULT', 'DIR', fallback=None)
+                    if dir is not None:
+                        dirs.append(os.path.join(subdir, dir))
+
+        return dirs
 
 
     def _extract(self, filename, subdir):
@@ -73,14 +110,24 @@ class PovrayWorker(object):
 
 
     def _download_extract_files(self):
-        if self._download_extract_file(self._image.model_id, 'Model') == False:
+        if self._download_extract_file(self._image.model_id,
+                                        'Model',
+                                        subdir=self.model_dir ) == False:
             return False
 
         for ffile in self._project.base_files:
-            if self._download_extract_file(ffile.id, 'BaseFiles', md5sum=ffile.md5sum) == False:
+            if self._download_extract_file(ffile.id,
+                                            'BaseFiles',
+                                            subdir=self.base_files_dir,
+                                            md5sum=ffile.md5sum) == False:
                 return False
 
         return True
+
+
+    def _povray_execute(self):
+        # povray model/master.ini -w640 -h480 -imodel/scene.pov -oscene.png
+        pass
 
 
     def run(self):
@@ -94,6 +141,12 @@ class PovrayWorker(object):
             print('All files downloaded correctly')
         else:
             print('Files are not downloaded correctly')
+
+        basefiles_dirs = self._read_input_directories(subdir=self.base_files_dir)
+
+
+        self._create_master_ini(basefiles_dirs)
+
         return False
 
 
@@ -108,5 +161,10 @@ if rq.login() == False:
 
 print('successfully logged into the RQ API')
 
-pw = PovrayWorker(rq, 'worker')
+
+default_libs = ['/Users/ocordes/software/PovrayCommandLineMacV2',
+                '/Users/ocordes/software/PovrayCommandLineMacV2/ini',
+                '/Users/ocordes/software/PovrayCommandLineMacV2/include']
+
+pw = PovrayWorker(rq, 'worker', default_libs=default_libs)
 ret = pw.run()
